9ac6f65cc89e4906aac50dce5e6d65e903bccf5b
annadif <128553523+annadif@users.noreply.github.com>
2024-12-29 18:54:18 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,152 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/23.png" alt="">
+      </div>
+
+      <div class="content">
+         <h3>CodeShope's Message:</h3>
+         <p>Hey There ! This is a final project built by BCA General Students [Batch : 2024] . Our website is designed exclusively for programmers, offering a curated selection of products tailored to meet the unique needs of coding professionals.</p>
+
+         <p>Code Shope is committed to providing a seamless shopping experience for developers, ensuring they have access to the latest and most efficient tools for their coding endeavors.</p>
+         <a href="contact.php" class="btn">Contact Us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+   
+   <h1 class="heading">Client's Reviews.</h1>
+
+   <div class="swiper reviews-slider">
+
+   <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide">
+         <img src="images/mk.jpg" alt="">
+         <p>Been using their services for quite a bit and have never had an issue with the quality of their products. Online e-products working great as well. Only issue I have is they usually deliver when I'm a little caught up, though I've set a preferred delivery time. Everything else has been good.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3> <a href="https://www.facebook.com/" target="_blank">Gabriel Jirbo</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/mk1.jpg" alt="">
+         <p>Codeshope has been an absolute game-changer for my coding journey. As an aspiring developer, finding the right tools and equipment is crucial for my success. From high-quality keyboards to ergonomic chairs, Codeshope has everything I need to create a comfortable and efficient workspace.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/" target="_blank">David Mabior</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/mk2.jpg" alt="">
+         <p>Code Shope is great if you choose good sellers . A variety of required item available . Customers can return and refund full amount within 7 days easily . Code Shope is boosting eCommerce business in Bangalore.It provides great opportunity to sale items online with ease.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/" target="_blank">Mahamat Abdelkerim</a></h3>
+      </div>
+
+      
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
